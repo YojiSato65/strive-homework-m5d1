bab2aae57fa5c90fdd7bef68b9e0f8b831fb2209
YojiSato65 <yojisato65@gmail.com>
2022-04-07 17:29:48 +0300

    added 'added' button

--- m5d1/src/components/SingleJob.jsx
+++ m5d1/src/components/SingleJob.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { addToFavsAction } from '../redux/actions'
 
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => ({
+  companyList: state.list.favorites,
+})
 
 const mapDispatchToProps = (dispatch) => ({
   addToFavs: (company) => {
@@ -11,20 +13,24 @@ const mapDispatchToProps = (dispatch) => ({
   },
 })
 
-const SingleJob = ({ job, addToFavs }) => {
+const SingleJob = ({ job, addToFavs, companyList }) => {
   return (
     <ListGroup.Item key={job._id}>
       <h4>{job.title}</h4>
       <div className="d-flex justify-content-between">
         <Link to={'/company=' + job.company_name}>{job.company_name}</Link>
-        <Button
-          variant="success"
-          onClick={() => {
-            addToFavs(job.company_name)
-          }}
-        >
-          Add to favorite
-        </Button>
+        {companyList.includes(job.company_name) ? (
+          <Button variant="warning">Added</Button>
+        ) : (
+          <Button
+            variant="success"
+            onClick={() => {
+              addToFavs(job.company_name)
+            }}
+          >
+            Add to favorite
+          </Button>
+        )}
       </div>
       <p className="mb-0">Area - {job.candidate_required_location}</p>
     </ListGroup.Item>
